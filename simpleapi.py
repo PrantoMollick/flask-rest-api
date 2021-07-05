a4e75b7ae6886d4d8731cf4f69b69161d0df48f3
Pranto Mollick <prantocse01@gmail.com>
2021-07-05 06:11:34 -0400

    basic rest api done

--- simpleapi.py
+++ simpleapi.py
@@ -2,46 +2,73 @@ from flask import Flask
 from flask_restful import Resource, Api
 from secure_check import authenticate, identity
 from flask_jwt import JWT, jwt_required
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+Migrate(app, db)
+
 api = Api(app)
 jwt = JWT(app, authenticate, identity)
 
+##############################################
+class Puppy(db.Model) :
+    name = db.Column(db.String(80), primary_key=True)
 
-{'name':'Rufus'}
+    def __init__(self, name):
+        self.name = name
+    
+    def json(self):
+        return {
+            'name': self.name
+        }
 
-puppies = []
+
+
+
+
+
+#############################################
 
 
 class PuppyNames(Resource):
 
-    @jwt_required()
+    # @jwt_required()
     def get(self, name):
-        for pup in puppies:
-            if pup['name'] == name:
-                return pup
         
-        return {'name':None}, 404
+        pup = Puppy.query.filter_by(name=name).first()
+
+        if pup:
+            return pup.json()
+        else:
+            return {'name':None}, 404
 
     def post(self, name):
-        pup = {'name': name}
-        puppies.append(pup)
-        return pup
+        pup = Puppy(name = name)
+        db.session.add(pup)
+        db.session.commit()
+        return pup.json()
 
     def delete(self, name):
-        for ind, pup in enumerate(puppies):
-            if pup['name'] == name:
-                delete_pup = puppies.pop(ind)
-                print(delete_pup)
-                return {'note':'Delete Success !'}
+        pup = Puppy.query.filter_by(name=name).first()
+        db.session.delete(pup)
+        db.session.commit()
+        return {'note':'Delete Success !'}
 
 
 class AllNames(Resource):
-    @jwt_required()
+    # @jwt_required()
     def get(self):
-        return {'puppies':puppies}
+        puppies = Puppy.query.all()
+
+        return [pup.json() for pup in puppies]
 
 
 
